c16324ec8727d24c60ede69abb49c60f9f70181b
ariel-u <foros.pokerlike@slmail.me>
2026-07-25 00:48:15 -0300

    update

---
+++ audio/.config/guitarix/banks/banklist.js
@@ -0,0 +1,7 @@
+[
+  ["Fender twin", "Fender twin.gx", 1, 0, [1, 2], 1784907733],
+  ["Bajo", "Bajo.gx", 1, 0, [1, 2], 1784907733],
+  ["Guitarra", "Guitarra.gx", 1, 0, [1, 2], 1784907733],
+  ["Base", "Base.gx", 1, 0, [1, 2], 1784907733],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1784907733]
+]
